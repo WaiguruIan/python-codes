aed54484cd12d6f433f60b2e7032b7f43d31c42e
Ian Waiguru <158230624+WaiguruIan@users.noreply.github.com>
2026-01-23 20:15:27 +0300

    Add files via upload

---
+++ grosspay.py
@@ -0,0 +1,17 @@
+#Adm NO: BSCIT-05-0096\2024
+#Name: Ian Waiguru
+
+Hours = int(input("Enter the hours: "))
+Rate = float(input("Enter the rate: "))
+Gross_pay = Hours * Rate
+
+print("The hours are ", Hours)
+print("The rate is ", Rate)
+print("The gross pay is ", Gross_pay)
+
+
+f = open ("C:\\Users\\Administrator\\Desktop\\Python\\gross.txt", "w")#w=write r=read a=append
+print ("Hours: ", Hours, file=f)
+print("Rate: ", Rate , file=f)
+print("Gross pay: ", Gross_pay, file=f)
+f.close()
---
+++ volume.py
@@ -0,0 +1,7 @@
+#Adm NO: BSCIT-05-0096\2024
+#Name: Ian Waiguru
+
+import math
+radius = int(input("Enter the radius of the sphere(cm): "))
+volume = 4/3 * math.pi*radius*radius*radius
+print(f"The volume of sphere with radius {radius} cm volume is ", volume)
