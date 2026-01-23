2b81e4637cd2290b83edcadc4bbb87e35b094582
Ian Waiguru <158230624+WaiguruIan@users.noreply.github.com>
2026-01-23 20:17:33 +0300

    Update volume.py

--- volume.py
+++ volume.py
@@ -1,7 +1,8 @@
-#Adm NO: BSCIT-05-0096\2024
+#Adm NO: BSCIT-05-0096/2024
 #Name: Ian Waiguru
 
 import math
 radius = int(input("Enter the radius of the sphere(cm): "))
 volume = 4/3 * math.pi*radius*radius*radius
 print(f"The volume of sphere with radius {radius} cm volume is ", volume)
+
